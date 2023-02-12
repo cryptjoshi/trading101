395e28cde121689ba662c9165a5b5e0dee9f9c71
itplaygroundth <itplayground@hotmail.com>
2023-02-13 01:47:11 +0700

    update binance config

---
+++ config.py
@@ -0,0 +1,6 @@
+apiKey = "9e6IO3MxiLmocPRWoFfuEU6ELFXKUvXU9wtVl2Q7GBIUM4nYVRWDOl51sTJ5CXEc"
+secretKey = "TJEw2oZPQxa3ma54z9EoxeZ9c0OIoJ2ht0KdeiJjTJ0zCEtpJMMIh0X43WrQtnxR"
+
+mailAddress = ""
+password = ""
+sendTo = ""
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 import pandas as pd
 import numpy as np
+from binance.client import Client
+from utils import readConfig,green,red
+
+Client = Client()
+
+def getdata(symbol,start,config):
+    api_key = str(config['api_key'])
+    api_secret = str(config['api_secret'])
+    frame  = pd.DataFrame(Client.get_historical_klines(symbol,'4h',start))
+    frame = frame.iloc[:,:6]
+    frame.columns = ['Time','Open','High','Low','Close','Volume']
+    frame.set_index('Time',inplace=True)
+    frame = frame.astype(float)
+    
+    return frame
+
+
+
+def main():
+    green('---------------------')
+    green('Pynance V.1 by CJ')
+    green('---------------------')
+    config = readConfig(r'Config.txt')
+    df = getdata('BTCUSDT','2021-01-01',config)
+    #df['SMA_50']
+    print(df)
+    #trading = Trading(config) #{'User':'Master','token':'xxxxxxxxxx','api_key':'yyyyyyyyyyy','api_secret':'zzzzzzzzzzz'})
+    
+if __name__ == '__main__':
+    main()
